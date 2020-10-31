dcccf7f5d1703e8cd86ec0aa698b5fcaa3541db3
Carrie <carriescott0@gmail.com>
2020-10-31 13:23:48 +0000

    Initial commit

---
+++ src/public/client.js
@@ -0,0 +1,189 @@
+// import {set} from "immutable";
+
+let store = {
+    user: { name: "Student" },
+    apod: '',
+    rover: '',
+    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    selectedRover: 'Curiosity',
+    roverInfo: {},
+    photos: ''
+};
+
+// add our markup to the page
+const root = document.getElementById('root');
+
+const updateStore = (store, newState) => {
+    store = Object.assign(store, newState)
+    render(root, store)
+}
+
+const render = async (root, state) => {
+    root.innerHTML = App(state)
+}
+
+function selectRover(rover_name) {
+    console.log(rover_name);
+    updateStore(store, { selectedRover: rover_name});
+    getRover(rover_name);
+    getRoverPhotos(store.rover.name, store.rover.max_date);
+}
+
+function Test(name) {
+    console.log(name);
+    updateStore(store, { selectedRover: name});
+    // const selected = document.getElementById(name);
+    // selected.classList.add('selected');
+}
+
+
+const Tab = (name, selectedRover) => {
+    if (name === selectedRover) {
+        console.log('selected test', name);
+    }
+
+    return (`
+        <button id="${name}" onclick="Test(id)" class=${name === selectedRover ? 'selected' : ''}>
+        ${name}
+        </button>
+        `)
+};
+
+const RoverTabs = (rovers, selectedRover) => {
+    return (`
+     ${rovers.map((name) => {
+        return (`
+       ${Tab(name, selectedRover)}
+        `)
+    }).join("")}
+    `)
+};
+
+// create content
+const App = (state) => {
+    let { rovers, selectedRover, roverInfo, rover } = state;
+
+    return `
+        <header>
+            ${RoverTabs(rovers, selectedRover)}
+        </header>
+        <main>
+            ${RoverData(selectedRover,rovers, roverInfo, rover)}
+        </main>
+        <footer></footer>
+    `
+}
+
+// listening for load event because page should load before any JS is called
+window.addEventListener('load', () => {
+    render(root, store)
+})
+
+// ------------------------------------------------------  COMPONENTS
+
+// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
+const Greeting = (name) => {
+    if (name) {
+        return `
+            <h1>Welcome, ${name}!</h1>
+        `
+    }
+    return `
+        <h1>Hello!</h1>
+    `
+}
+
+
+
+// Example of a pure function that renders information requested from the backend
+const ImageOfTheDay = (apod) => {
+    // If image does not already exist, or it is not from today -- request it again
+    const today = new Date();
+    const photodate = new Date(apod.date);
+    console.log(photodate.getDate(), today.getDate());
+
+    console.log(photodate.getDate() === today.getDate());
+    if (!apod || apod.date === today.getDate() ) {
+        getImageOfTheDay(store)
+    }
+    // check if the photo of the day is actually type video!
+    if (apod.media_type === "video") {
+        return (`
+            <p>See today's featured video <a href="${apod.url}">here</a></p>
+            <p>${apod.title}</p>
+            <p>${apod.explanation}</p>
+        `)
+    } else {
+        return (`
+            <img src="${apod.image.url}" height="350px" width="100%" />
+            <p>${apod.image.explanation}</p>
+        `)
+    }
+};
+
+
+
+const RoverData = (selectedRover, rovers, roverInfo, rover) => {
+    console.log(selectedRover);
+    if(rover === '') {
+        getRover(selectedRover);
+    }
+    console.log('rover', rover);
+    return(` 
+        <h3>${rover.name}</h3>
+        <p>${rover.landing_date}</p>
+        <p>${rover.launch_date}</p>
+        <p>${rover.status}</p>
+        <p>${rover.max_sol}</p>
+        <p>${rover.max_date}</p>
+    `)
+};
+
+
+const RoverPhotos = (selectedRover, rover, photos) => {
+    console.log('hello');
+    if(photos === '') {
+        console.log(rover.max_date);
+        getRoverPhotos(selectedRover, rover.max_date);
+    }
+    return(` 
+        <p>${photos[0].id}</p>
+         <img src="${photos[0].img_src}" height="350px" width="100%" />
+        <p>${photos.length}</p>
+        <p>${photos[1].id}</p>
+    `)
+};
+
+// ------------------------------------------------------  API CALLS
+
+const getImageOfTheDay = (state) => {
+    let { apod } = state;
+    fetch(`http://localhost:3000/apod`)
+        .then(res => res.json())
+        .then(apod => updateStore(store, { apod }))
+};
+
+const getRover = (rover_name) => {
+    fetch(`http://localhost:3000/rovers/${rover_name}`)
+        .then(res => res.json())
+        .then(({ photo_manifest })=> updateStore(store, {
+            rover: {
+                ...photo_manifest
+            }
+        })
+        )
+};
+
+const getRoverPhotos = (rover_name, max_date) => {
+    console.log(max_date);
+    fetch(`http://localhost:3000/rover_photos/${rover_name}/${max_date}`)
+        .then(res => res.json())
+        .then(( {photos} ) => {
+            updateStore(store, {
+                    photos: [
+                        ...photos,
+                    ]
+                }
+            )})
+};
+
---
+++ src/server/index.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const rover_ep = 'https://api.nasa.gov/mars-photos/api/v1';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+// your API calls
+
+// example API call
+app.get('/apod', async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+            .then(res =>
+            res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+});
+
+app.get('/rovers/:rover_name', async (req, res) => {
+    try {
+        let rover = await fetch(`
+         ${rover_ep}/manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}
+         `).then(res =>
+                res.json());
+        console.log('rover res', rover);
+        res.send(rover);
+    } catch (err) {
+        console.log('error:', err);
+    }
+});
+
+app.get('/rover_photos/:rover_name/:max_date', async (req, res) => {
+    console.log('req', req);
+    try {
+        let rover_photos = await fetch(
+            `${rover_ep}/rovers/${req.params.rover_name}/photos?earth_date=${req.params.max_date}&api_key=${process.env.API_KEY}
+        `).then(res => res.json());
+        console.log('rover res', rover_photos);
+        res.send(rover_photos)
+    } catch (err) {
+        console.log('error: ', err)
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
